6088f39d575940f31e26c954d624ccbf5bb73970
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-12-26 11:53:46 -0600

    Create and complete Sammenlign.java for trix 01.06

---
+++ trix_oppgaver/uke1/Sammenlign.java
@@ -0,0 +1,14 @@
+class Sammenlign {
+    public static void main(String[] args){
+        //Declare 2 variables: a and b
+        int a = 21; int b = 20;
+
+        //Determine which is larger
+        if (a > b) {
+            System.out.println("a er storre enn b");
+        }
+        else {
+            System.out.println("a er ikke storre enn b");
+        }
+    }
+}
