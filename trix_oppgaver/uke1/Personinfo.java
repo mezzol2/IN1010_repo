7ba41debf7d77550e409fd0abf26933bff3b77f4
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2024-12-26 11:45:44 -0600

    Change arg to args

--- trix_oppgaver/uke1/Personinfo.java
+++ trix_oppgaver/uke1/Personinfo.java
@@ -1,5 +1,5 @@
 class Personinfo {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
         System.out.println("John Doe");
         System.out.println("999 99 999");
         System.out.println("123 Main St.");
