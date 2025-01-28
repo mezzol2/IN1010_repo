63a34a99111b42698b09be7d65a401c807b50f4b
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-01-28 21:32:25 +0100

    Create and complete trix 02.02

--- trix_oppgaver/uke2/myFirstHashMap.java
+++ trix_oppgaver/uke2/myFirstHashMap.java
@@ -12,5 +12,28 @@ class myFirstHashMap{
         for (int i : myHashMap.keySet()) {
             System.out.println(i + ":" + myHashMap.get(i));
         }
+
+        System.out.println(myHashMap.get(5));
+        myHashMap.put(3, "new c!");
+        System.out.println("New 3 value: "+myHashMap.get(3));
+
+        myHashMap.remove(1);
+        System.out.println(myHashMap.get(1));
+        myHashMap.remove(1);
+        System.out.println(myHashMap.get(1));
+
+        for (String s: myHashMap.values()){
+            System.out.println(s);}
+        System.out.println();
+        System.out.println(myHashMap);
+        System.out.println(myHashMap.entrySet());
+
+        for (HashMap.Entry<Integer, String> entry : myHashMap.entrySet()){
+            System.out.println(entry.getKey());
+        }
+
+        for (HashMap.Entry<Integer, String> entry: myHashMap.entrySet()){
+            System.out.println(entry.getValue());
+        }
     }
 }
