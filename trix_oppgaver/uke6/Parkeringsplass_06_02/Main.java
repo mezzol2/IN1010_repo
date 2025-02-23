41d8629f21b28e4df2d2a49f96a17ae5410fd7e5
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-02-23 12:59:58 +0100

    Initialize 3 more Bil and 3 more Motorsykkel instances in the Main program.

--- trix_oppgaver/uke6/Parkeringsplass_06_02/Main.java
+++ trix_oppgaver/uke6/Parkeringsplass_06_02/Main.java
@@ -11,5 +11,16 @@ class Main{
 
         System.out.println(carPlace.driveOff().getRegNum());
         System.out.println(bikePlace.driveOff().getRegNum());
+
+        //Test Parkeringshus
+        Bil bil1 = new Bil("111", 1);
+        Bil bil2 = new Bil("222",2);
+        Bil bil3 = new Bil("333",3);
+        Motorsykkel bike1 = new Motorsykkel("444", 40);
+        Motorsykkel bike2 = new Motorsykkel("555", 50);
+        Motorsykkel bike3 = new Motorsykkel("666", 60);
+
+
+
     }
 }
